bde3126618ab6a775d53743fb8237db47a65d249
Hamciarka112 <patrikezas@gmail.com>
2024-12-04 23:08:39 +0100

    Update fxmanifest.lua

--- ham-snowball/fxmanifest.lua
+++ ham-snowball/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 description 'Ham-snowball'
 lua54 'yes'
-version '1.1' -- [FIX] Padaryta kad gautum itema kaip gniukste , pridedas checkas kad tikrintu ar turi vietos savo inv , bsk optimizacija padaryta, pakeistas zonu atsiradimo placementas padaryta labiau random.
+version '1.0' 
 
 
 shared_scripts {
@@ -25,8 +25,4 @@ dependencies {
     'ox_target',
     'es_extended',
     'ox_lib'
-}
-
-escrow_ignore {
-    'config.lua'
 }
